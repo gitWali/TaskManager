48e3b82fa0a6a5ff7958a440f3b7797a966fcf47
H.Waligorski <146819340+gitWali@users.noreply.github.com>
2026-07-29 09:55:02 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,13 @@
 
+package service;
+
+import model.Priority;
+import model.Task;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager 
+{
+  
+}
