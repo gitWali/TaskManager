bd43c430b80d50ee965560836ccb46ea261b0996
H.Waligorski <146819340+gitWali@users.noreply.github.com>
2026-07-29 09:59:26 +0200

    Update taskManager.java

--- taskManager.java
+++ taskManager.java
@@ -1,1 +1,49 @@
 
+package service;
+
+import model.Priority;
+import model.Task;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager 
+{
+    private final List<Task> tasks = new ArrayList<>();
+    private int nextId = 1; 
+  
+    public Task createTask(String title, String description, Priority priority, LocalDate dueDate) 
+  {
+        Task newTask = new Task(nextId++, title, description, priority, dueDate);
+        tasks.add(newTask);
+        return newTask;
+    }
+  
+    public List<Task> getAllTasks() 
+    {
+        return new ArrayList<>(tasks);
+    }
+
+    public boolean completeTask(int id) 
+  {
+        Task task = findById(id);
+        if (task != null) 
+        {
+            task.setCompleted(true);
+            return true;
+        }
+        return false;
+    }
+
+    public Task findById(int id) 
+  {
+        for (Task task : tasks) 
+        {
+            if (task.getId() == id) 
+            {
+                return task;
+            }
+        }
+        return null;
+    }
+}
