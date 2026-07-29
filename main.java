adfcc93ae53885abf1f0d0236885c5d8db44571f
H.Waligorski <146819340+gitWali@users.noreply.github.com>
2026-07-29 10:08:15 +0200

    Update main.java

--- main.java
+++ main.java
@@ -13,16 +13,29 @@ public class Main
         System.out.println("=== STARTING MINI-JIRA CLI ===");
 
         TaskManager manager = new TaskManager();
+        FileHandler fileHandler = new FileHandler("task.csv");
+
+        List<Task> existingTasks = fileHandler.loadTasks();
+        manager.importLoadedTasks(existingTasks);
+
+        MenuConsole menu = new MenuConsole(manager);
+        menu.start();
+
+        System.out.println("\n[Shutdown] Sichere Daten auf Festplatte...");
+        fileHandler.saveTasks(manager.getAllTasks());
+        System.out.println("=== MINI-JIRA ERFOLGREICH BEENDET ===");
 
         System.out.println("\n[System] Erstelle Jira-Tickets im RAM...");
-        manager.createTask(
+        manager.createTask
+        (
             "Repository einrichten", 
             "Git-Repository erstellen und die README.md hinzufügen.", 
             Priority.HIGH, 
             LocalDate.now().plusDays(1)
         );
 
-        manager.createTask(
+        manager.createTask
+        (
             "Datenmodell implementieren", 
             "Klassen für Task und Priority-Enum in Java schreiben.", 
             Priority.MEDIUM, 
